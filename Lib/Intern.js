de22ffaa0ae6ca71d88faaf43f1cfdd0ff5ec057
Nicholas Cortes <nicholascortes@Nicholass-MacBook-Air.local>
2022-10-25 21:23:29 -0700

    added remaining class

---
+++ Lib/Intern.js
@@ -0,0 +1,20 @@
+class Intern {
+    constructor (id, name, email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+    getName() {
+        return this.name
+    }
+    getId(){
+    return this.id
+    }
+    getEmail() {
+        return this.email
+    }
+    getRole() {
+        return "Employee"
+    }
+}
+module.exports = Intern
